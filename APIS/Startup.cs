c00b2f591df501b803e7622d23171ecf5e4ba38a
dtrygodko <dtrygodko@ukr.net>
2016-12-04 17:09:44 +0200

    first commit

---
+++ APIS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APIS.Startup))]
+namespace APIS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
