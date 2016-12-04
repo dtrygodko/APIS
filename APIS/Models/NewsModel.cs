6dff2037972994e0a747666d6d5ec743fb92acaa
dtrygodko <dtrygodko@ukr.net>
2016-12-04 19:33:42 +0200

    Added news model, controller, test view

---
+++ APIS/Models/NewsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIS.Models
+{
+    public class NewsModel
+    {
+        public string Text { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
